9c5a7d1189e44a4c0fac48d1330e7c600b83dc6a
me <me@me.me.com>
2020-05-05 13:50:32 -0700

    implement the first spec

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function decimalToRomanNumeral(equivalent){
+  
+  var equivalentsMap = new Map([[1,"I"],[5,"V"],[10,"X"],[50,"L"],[100,"C"],[500,"D"],[1000,"M"]]);
+  return equivalentsMap.get(equivalent);
+}
+
+
+$(document).ready(function(){
+
+  console.log(decimalToRomanNumeral(5));
+})
